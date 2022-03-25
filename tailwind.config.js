55546d1af4c3b2628e1ff794372e814fb5d213c2
MuradCade <king12murad@gmail.com>
2022-03-24 19:44:31 -0700

    change was made

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,13 @@ module.exports = {
     './public/index.html',
     './src/**/*.{html,js}',
   ],
-  
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
-      'main':'#f1faee',
-      'button':'#ebf4f5',
-   
-    }
-  }
+    extend: {
+      // ...
+    },
+  },
+
+ 
     // ...
-}
+};
+
